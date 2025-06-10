77621dc22609a835dd688ce1b007168cd294e4c9
Carlos Henrique <carlos.rique23@gmail.com>
2025-06-10 19:01:20 -0300

    Transforming the User class into an entity

--- estudos/src/main/java/depaiva/com/estudos/models/User.java
+++ estudos/src/main/java/depaiva/com/estudos/models/User.java
@@ -1,8 +1,31 @@
 package depaiva.com.estudos.models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
 public class User {
+
+    @Id
+    @Column
+    private String id;
+
+    @Column
     private String nome;
+
+    @Column
     private String age;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 
     public String getnome() {
         return nome;
@@ -23,5 +46,5 @@ public class User {
     @Override
     public String toString() {
         return "User [nome=" + nome + ", age=" + age + "]";
-    }    
+    }
 }
